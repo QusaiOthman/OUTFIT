54b4487b639bfef646d230a62abcdd07c6aa6893
Qusai Othman <othmanq30@gmail.com>
2026-06-06 01:08:07 +0300

    modify homepage products

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,8 +10,8 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $products = Product::with('category', 'images')->latest()->take(8)->get();
-        $categories = Category::withCount('products')->take(4)->get();
+        $products = Product::with('category', 'images')->inRandomOrder()->take(12)->get();
+        $categories = Category::withCount('products')->inRandomOrder()->take(4)->get();
 
         return view('home', compact('products', 'categories'));
     }
